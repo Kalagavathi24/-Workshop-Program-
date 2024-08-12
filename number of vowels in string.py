fa9f2176f11a3db180e3df5e448347799a9089af
Kalagavathi24 <kalagavathiselvaraj24@gmail.com>
2024-08-12 20:15:26 +0530

    Create number of vowels in string.py

---
+++ number of vowels in string.py
@@ -0,0 +1,6 @@
+[7:58 PM, 8/12/2024] Kalagavathi: name = ['k', 'a', 'l', 'a', 'g', 'a', 'v', 'a', 't', 'h', 'i']
+vowels = 0
+for char in name:
+    if char in 'aeiouAEIOU':
+        vowels += 1
+print("The number of vowels in the string is:", vowels)
