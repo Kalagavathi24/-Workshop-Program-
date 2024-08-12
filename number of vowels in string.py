a4baf872ff0a426f3747c1b4254f73f9b9a96b5e
Kalagavathi24 <kalagavathiselvaraj24@gmail.com>
2024-08-12 20:16:22 +0530

    Update number of vowels in string.py

--- number of vowels in string.py
+++ number of vowels in string.py
@@ -1,4 +1,4 @@
-[7:58 PM, 8/12/2024] Kalagavathi: name = ['k', 'a', 'l', 'a', 'g', 'a', 'v', 'a', 't', 'h', 'i']
+ name = ['k', 'a', 'l', 'a', 'g', 'a', 'v', 'a', 't', 'h', 'i']
 vowels = 0
 for char in name:
     if char in 'aeiouAEIOU':
