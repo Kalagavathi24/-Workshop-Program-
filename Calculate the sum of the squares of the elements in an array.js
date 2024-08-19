96415f20c681dc3c985ec6d47f7d136d07b25bfe
Kalagavathi24 <kalagavathiselvaraj24@gmail.com>
2024-08-19 20:48:15 +0530

    Calculate the sum of the squares of the elements in an array.js

---
+++ Calculate the sum of the squares of the elements in an array.js
@@ -0,0 +1,6 @@
+let array = [2, 3, 4];
+var sumOfSquares = 0;
+for (let i = 0; i < array.length; i++) {
+    sumOfSquares += array[i] * array[i];
+}
+console.log("Sum of squares of the elements:", sumOfSquares);
